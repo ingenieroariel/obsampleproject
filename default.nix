6c00b09bc05fb162cccbd05952eccd000438a7bf
Ariel Nunez <ingenieroariel@gmail.com>
2019-12-19 21:14:43 +0000

    Use virtualbox instead of amazon

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { obelisk ? import ./.obelisk/impl {
     system = builtins.currentSystem;
     iosSdkVersion = "10.2";
+    defaultVirtualizationHost = "virtualbox";
     # You must accept the Android Software Development Kit License Agreement at
     # https://developer.android.com/studio/terms in order to build Android apps.
     # Uncomment and set this to `true` to indicate your acceptance:
